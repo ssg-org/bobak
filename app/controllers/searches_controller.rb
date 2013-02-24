1629a971e4cb267f82d4f36e250e7c6838bd7ee3
Samir Eljazovic <samir.eljazovic@gmail.com>
2013-02-24 02:42:56 +0100

    Implement limit and offset on DB level

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,5 +1,10 @@
 class SearchesController < ApplicationController
+	MAX_LIMIT = 20
+
 	def show
-		@owners = Owner.search(params[:q], params[:o], params[:l])
+		limit = params[:l].to_i > 0 ? [MAX_LIMIT, params[:l].to_i] : MAX_LIMIT 
+		offset = params[:o].to_i
+
+		@owners = Owner.search(params[:q], offset, limit)
 	end
 end
--- app/models/owner.rb
+++ app/models/owner.rb
@@ -8,22 +8,18 @@ class Owner < ActiveRecord::Base
 
   # Free text search
   pg_search_scope :search_full_text, 
-  								:against => [:oid, :name],
+  								:against => :name,
   								:using => {
+                      trigram: {},
 	                    tsearch: {
 	                      tsvector_column: 'full_text'
   	                  }
   									}
 
-  # Fuzzy search
-  pg_search_scope :search_trigram,
-                  :against => :name,
-                  :using => :trigram
-
   def self.search(query, offset = 0, limit = 10)
     off = offset.blank? ? 0 : offset.to_i
     lim = limit.blank? ? 10 : limit.to_i
 
-    return (self.search_full_text(query) + self.search_trigram(query)).slice(off, lim)
+    return self.search_full_text(query).offset(off).limit(lim)
   end
 end
