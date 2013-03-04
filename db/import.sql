d4f45a5ff4fe74a5709929c3d6d7e732aba9a362
Samir Eljazovic <samir.eljazovic@gmail.com>
2013-03-04 21:30:28 +0100

    Added parsed date and summary tables

--- db/import.sql
+++ db/import.sql
@@ -6,16 +6,20 @@
 
 set client_encoding to 'UTF8';
 
+/*
 TRUNCATE TABLE banks;
 TRUNCATE TABLE owners;
 TRUNCATE TABLE accounts;
 TRUNCATE TABLE account_statuses;
+TRUNCATE TABLE bank_summaries;
+TRUNCATE TABLE owner_summaries;
+*/
 
 -- Load raw data
 drop table :raw_table;
 create table :raw_table (id varchar(255), account varchar(255), name text, bank varchar(255));
 
-\copy januar from db/export/2013-01-03.csv csv header
+\copy februar from db/export/2013-02-01.csv csv header
 
 -- BANKS
 ALTER TABLE ONLY banks ALTER COLUMN created_at SET DEFAULT current_timestamp;
@@ -75,9 +79,43 @@ ALTER TABLE ONLY accounts ALTER COLUMN updated_at SET DEFAULT null;
 ALTER TABLE ONLY account_statuses ALTER COLUMN created_at SET DEFAULT current_timestamp;
 ALTER TABLE ONLY account_statuses ALTER COLUMN updated_at SET DEFAULT current_timestamp;
 
-insert into account_statuses(account_id, date, status)
-(select accounts.id, :date, 0
+insert into account_statuses(account_id, date, year, month, day, status)
+(select accounts.id, :date, date_part('year',DATE :date), date_part('month',DATE :date), date_part('day',DATE :date), 0
 	from :raw_table inner join accounts on :raw_table.account = accounts.number);
 
 ALTER TABLE ONLY account_statuses ALTER COLUMN created_at SET DEFAULT null;
 ALTER TABLE ONLY account_statuses ALTER COLUMN updated_at SET DEFAULT null;
+
+-- Bank summaries
+ALTER TABLE ONLY bank_summaries ALTER COLUMN created_at SET DEFAULT current_timestamp;
+ALTER TABLE ONLY bank_summaries ALTER COLUMN updated_at SET DEFAULT current_timestamp;
+
+INSERT INTO bank_summaries(bank_id, date, year, month, day, blocked_accounts, blocked_owners)
+(SELECT
+	banks.id, account_statuses.date, account_statuses.year, account_statuses.month, account_statuses.day, count(distinct accounts.id), count(distinct accounts.owner_id)
+ FROM banks 
+ 	INNER JOIN accounts on banks.id = accounts.bank_id
+ 	INNER JOIN account_statuses on accounts.id = account_statuses.account_id
+ WHERE
+ 	account_statuses.date = DATE :date
+ GROUP BY banks.id, account_statuses.date, account_statuses.year, account_statuses.month, account_statuses.day);
+
+ALTER TABLE ONLY bank_summaries ALTER COLUMN created_at SET DEFAULT null;
+ALTER TABLE ONLY bank_summaries ALTER COLUMN updated_at SET DEFAULT null;
+
+
+-- Account summaries
+ALTER TABLE ONLY owner_summaries ALTER COLUMN created_at SET DEFAULT current_timestamp;
+ALTER TABLE ONLY owner_summaries ALTER COLUMN updated_at SET DEFAULT current_timestamp;
+
+INSERT INTO owner_summaries(owner_id, date, year, month, day, blocked_accounts)
+(SELECT
+	accounts.owner_id, account_statuses.date, account_statuses.year, account_statuses.month, account_statuses.day, count(distinct accounts.id)
+ FROM accounts 
+ 	INNER JOIN account_statuses on accounts.id = account_statuses.account_id
+ WHERE
+ 	account_statuses.date = DATE :date
+ GROUP BY accounts.id, account_statuses.date, account_statuses.year, account_statuses.month, account_statuses.day);
+
+ALTER TABLE ONLY owner_summaries ALTER COLUMN created_at SET DEFAULT null;
+ALTER TABLE ONLY owner_summaries ALTER COLUMN updated_at SET DEFAULT null;
