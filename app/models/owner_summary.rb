4917fe683c7db10ecce40c913f44c465155a8a63
Samir Eljazovic <samir.eljazovic@gmail.com>
2013-03-09 18:17:35 +0100

    Remove special accounts and sort by owner name

--- app/models/owner_summary.rb
+++ app/models/owner_summary.rb
@@ -4,6 +4,6 @@ class OwnerSummary < ActiveRecord::Base
 	belongs_to :owner
 
 	def self.top_owners_by_accounts(limit, month)
-  	return OwnerSummary.includes(:owner).order('blocked_accounts DESC').limit(limit).where(:month => month)
+  	return OwnerSummary.includes(:owner).order('blocked_accounts DESC, owners.name').limit(limit).where(:month => month).where('owners.oid is not null')
 	end
 end
